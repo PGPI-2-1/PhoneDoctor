60a9753804fadf7d5a08d1070a570635291f853d
antpelmor@alum.us.es <antpelmor@alum.us.es>
2023-11-15 18:41:37 +0100

    feat:Login con email

--- PhoneDoctor/PhoneDoctor/settings.py
+++ PhoneDoctor/PhoneDoctor/settings.py
@@ -38,8 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'user',
+    'django_use_email_as_username.apps.DjangoUseEmailAsUsernameConfig',
+    'custom_user.apps.CustomUserConfig',
 ]
+
+AUTH_USER_MODEL = 'custom_user.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- PhoneDoctor/user/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from user.models import User
-
-admin.site.register(User)
