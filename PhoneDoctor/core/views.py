1764caaafd4ba7a7cc19face0e398fdf62dae555
Juan <juanmc015@gmail.com>
2023-11-20 11:55:44 +0100

    feat: search done

--- PhoneDoctor/core/views.py
+++ PhoneDoctor/core/views.py
@@ -8,6 +8,7 @@ def index(request):
 
     category = request.GET.get('category', None)
     brand = request.GET.get('brand', None)
+    query = request.GET.get('q')
 
     if category and brand:
         products = Product.objects.filter(brand__name=brand, category__name=category)
@@ -15,6 +16,11 @@ def index(request):
         products = Product.objects.filter(category__name=category)
     elif brand:
         products = Product.objects.filter(brand__name=brand)
+    elif query: 
+        products = Product.objects.filter(name__icontains=query)
+       
+   
+
 
     return render(request, 'core/index.html', {
         'categories': categories,
