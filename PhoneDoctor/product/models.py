1fe053f3520ea2c5e698d2a178ebfb16feeb5586
Juan <juanmc015@gmail.com>
2023-11-21 08:01:42 +0100

    fix: populate images are now showing

--- PhoneDoctor/product/models.py
+++ PhoneDoctor/product/models.py
@@ -29,7 +29,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description=models.TextField(blank=True,null=True)
     price=models.FloatField()
-    image = models.ImageField(upload_to='item_images', blank=True, null=True)
+    image = models.ImageField(blank=True, null=True)
     is_sold=models.BooleanField(default=False)
 
     def __str__(self):
