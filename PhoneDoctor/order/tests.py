ded487c7f3667d2cd7bbfc213da57c36550dd40b
antpelmor@alum.us.es <antpelmor@alum.us.es>
2023-11-27 18:53:15 +0100

    test: unit test case for shop.cart and order logic

--- PhoneDoctor/order/tests.py
+++ PhoneDoctor/order/tests.py
@@ -1,3 +1,62 @@
-from django.test import TestCase
+# orders/tests.py
 
-# Create your tests here.
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from product.models import Product, Category, Brand
+from shoppingCart.models import CartItem
+from .models import Order
+
+class OrderModelTest(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name='Test Category')
+        self.brand = Brand.objects.create(name='Test Brand')
+        self.user = get_user_model().objects.create_user(email='testuser@example.com', password='testpassword')
+
+    def test_order_creation(self):
+        product = Product.objects.create(name='Test Product', price=10.0, brand=self.brand, category=self.category, quantity=5)
+        cart_item = CartItem.objects.create(user=self.user, product=product, quantity=2, is_processed=False)
+        
+        order = Order.objects.create(user=self.user, address='Test Address', email='test@example.com', status=Order.StatusChoices.PAGADO, precio_total=20.0)
+        order.items.add(cart_item)
+
+        self.assertEqual(order.user, self.user)
+        self.assertEqual(order.address, 'Test Address')
+        self.assertEqual(order.email, 'test@example.com')
+        self.assertEqual(order.status, Order.StatusChoices.PAGADO)
+        self.assertEqual(order.precio_total, 20.0)
+        self.assertEqual(order.items.count(), 1)
+
+class OrderViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = get_user_model().objects.create_user(email='testuser@example.com', password='testpassword')
+        self.category = Category.objects.create(name='Test Category')
+        self.brand = Brand.objects.create(name='Test Brand')
+
+    def test_checkout_authenticated_user(self):
+        product = Product.objects.create(name='Test Product', price=10.0, brand=self.brand, category=self.category, quantity=5)
+        cart_item = CartItem.objects.create(user=self.user, product=product, quantity=2, is_processed=False)
+        
+        self.client.login(email='testuser@example.com', password='testpassword')
+        response = self.client.post(reverse('checkout'), {'address': 'Test Address'})
+        
+        self.assertEqual(response.status_code, 302) 
+
+        order = Order.objects.get(user=self.user)
+        self.assertIsNotNone(order)
+        self.assertEqual(order.items.count(), 1)
+        self.assertTrue(all(cart_item.is_processed for cart_item in CartItem.objects.filter(user=self.user)))
+
+    def test_checkout_unauthenticated_user(self):
+        product = Product.objects.create(name='Test Product', price=10.0, brand=self.brand, category=self.category, quantity=5)
+        cart_item = CartItem.objects.create(user=None, product=product, quantity=2, is_processed=False)
+
+        response = self.client.post(reverse('checkout'), {'address': 'Test Address', 'email': 'test@example.com'})
+        
+        self.assertEqual(response.status_code, 302)
+
+        order = Order.objects.get(user=None)
+        self.assertIsNotNone(order)
+        self.assertEqual(order.items.count(), 1)
+        self.assertTrue(all(cart_item.is_processed for cart_item in CartItem.objects.filter(user=None)))
--- PhoneDoctor/shoppingCart/tests.py
+++ PhoneDoctor/shoppingCart/tests.py
@@ -1,3 +1,65 @@
-from django.test import TestCase
+# En tu archivo tests.py
 
-# Create your tests here.
+from django.test import TestCase, Client
+from django.urls import reverse
+from custom_user.models import User
+from product.models import Product, Category, Brand
+from .models import CartItem
+
+class CartItemModelTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.category = Category.objects.create(name='Test Category')
+        self.brand = Brand.objects.create(name='Test Brand')
+    def test_cart_item_creation(self):
+        user = User.objects.create(email='testuser@example.com', password='testpassword')
+        product = Product.objects.create(name='Test Product', price=10.0, brand=self.brand, category=self.category)
+        cart_item = CartItem.objects.create(user=user, product=product, quantity=2, is_processed=False)
+
+        self.assertEqual(cart_item.user, user)
+        self.assertEqual(cart_item.product, product)
+        self.assertEqual(cart_item.quantity, 2)
+        self.assertFalse(cart_item.is_processed)
+
+class CartItemViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(email='testuser@example.com', password='testpassword')
+        self.category = Category.objects.create(name='Test Category')
+        self.brand = Brand.objects.create(name='Test Brand')
+        self.product = Product.objects.create(name='Test Product', price=10.0, brand=self.brand, category=self.category, quantity=10)
+    def test_add_to_cart_quantity_exceeded(self):
+        product = Product.objects.create(name='Test Product', price=10.0, brand=self.brand, category=self.category, quantity=5)
+        self.client.login(email='testuser@example.com', password='testpassword')
+
+        response = self.client.get(reverse('add_to_cart', args=[product.id]), {'cantidad': 10})
+
+        cart_item = CartItem.objects.filter(user=self.user, product=product).first()
+        self.assertIsNone(cart_item)
+
+    def test_add_to_cart_quantity_zero(self):
+        product = Product.objects.create(name='Test Product', price=10.0, brand=self.brand, category=self.category, quantity=10)
+        self.client.login(email='testuser@example.com', password='testpassword')
+
+        response = self.client.get(reverse('add_to_cart', args=[product.id]), {'cantidad': 0})
+
+        cart_item = CartItem.objects.filter(user=self.user, product=product).first()
+        self.assertIsNone(cart_item)
+
+    def test_add_to_cart_authenticated_user(self):
+        self.client.login(email='testuser@example.com', password='testpassword')
+        response = self.client.get(reverse('add_to_cart', args=[self.product.id]), {'cantidad': 3})
+        self.assertEqual(response.status_code, 302)
+
+        cart_item = CartItem.objects.get(user=self.user, product=self.product)
+        self.assertEqual(cart_item.quantity, 3)
+
+    def test_add_to_cart_unauthenticated_user(self):
+
+        response = self.client.get(reverse('add_to_cart', args=[self.product.id]), {'cantidad': 2})
+        self.assertEqual(response.status_code, 302)
+
+        cart_item = CartItem.objects.get(user=None, product=self.product)
+        self.assertEqual(cart_item.quantity, 2)
+
+    
