c8a33cdde2ab07b868e4bc834ead824533a78cdf
Juan <juanmc015@gmail.com>
2023-12-02 16:37:21 +0100

    feat: add about us view

--- PhoneDoctor/core/urls.py
+++ PhoneDoctor/core/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('category/<str:category>/', views.index, name='index_category'),
     path('brand/<str:brand>/', views.index, name='index_brand'),
+    path('about-us/',views.about_us,name = 'about_us')
 ]
